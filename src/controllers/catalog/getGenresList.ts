560fd8147dbd7d52cdfca2d6f967bbfdfd267438
Alex Dudnikov <alekdudnikov@mail.ru>
2022-08-31 18:38:04 +0300

    changed get gere list

--- src/controllers/catalog/getGenresList.ts
+++ src/controllers/catalog/getGenresList.ts
@@ -1,15 +1,12 @@
 import { RequestHandler } from 'express-serve-static-core';
 import { StatusCodes } from 'http-status-codes';
-import { genreRepository, booksRepository, usersRepository } from '../../db';
+import { genreRepository, booksRepository } from '../../db';
 import { Book } from '../../db/entity/Book';
 import { Genre } from '../../db/entity/Genre';
-import { User } from '../../db/entity/User';
-import jwtTools, { Payload } from '../../utils/authTools';
 
 type Response = {
   genres: Genre[];
   books: Book[];
-  user: User;
 }
 
 type ControllerType = RequestHandler<
@@ -17,13 +14,6 @@ Record<string, never>, Response, Record<string, never>>
 
 const getGenreList: ControllerType = async (request, response, next) => {
   try {
-    let user = null;
-    if (request.headers.authorization) {
-      const token = request.headers.authorization.split(' ')[1];
-      const tokenPayload = jwtTools.validateAccessToken(token) as Payload;
-      const id = tokenPayload.id;
-      user = await usersRepository.findOneBy({ id });
-    }
     const genres = await genreRepository.find();
     // const books = await booksRepository.find();
     const books = await booksRepository.find({
@@ -37,7 +27,7 @@ const getGenreList: ControllerType = async (request, response, next) => {
       return (a, b) => (a[field] < b[field] ? 1 : -1);
     };
     books.sort(byField('middleRating'));
-    response.status(StatusCodes.ACCEPTED).json({ genres, books, user });
+    response.status(StatusCodes.ACCEPTED).json({ genres, books });
   } catch (err) {
     next(err);
   }
